183c048949cf8ef0cd81aa29309e4385c0df6908
박병천 <genius178896@gmail.com>
2024-12-11 13:42:30 +0900

    chore : 일정 내용 변경을 위한 요청Dto 생성

---
+++ src/main/java/pbc/schedule/dto/request/UpdatedRequestScheduleDto.java
@@ -0,0 +1,10 @@
+package pbc.schedule.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UpdatedRequestScheduleDto {
+    private String content;
+}
