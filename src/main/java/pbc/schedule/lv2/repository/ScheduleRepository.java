244d595f2a40d19d84a6464995d38de92b49d51e
박병천 <genius178896@gmail.com>
2024-12-11 16:05:37 +0900

    chore : 패키지 삭제

--- src/main/java/pbc/schedule/lv2/repository/ScheduleRepository.java
+++
@@ -1,7 +0,0 @@
-package pbc.schedule.lv2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pbc.schedule.lv2.entity.Schedule;
-
-public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-}
