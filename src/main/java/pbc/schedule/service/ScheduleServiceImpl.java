a7e2348b24badea39c96474f104ec81ba3175651
박병천 <genius178896@gmail.com>
2024-12-11 11:43:55 +0900

    feat : ScheduleServiceImpl 할 일 생성 기능 추가

--- src/main/java/pbc/schedule/service/ScheduleServiceImpl.java
+++ src/main/java/pbc/schedule/service/ScheduleServiceImpl.java
@@ -3,10 +3,19 @@ package pbc.schedule.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pbc.schedule.dto.ScheduleResponseDto;
+import pbc.schedule.entity.Schedule;
 import pbc.schedule.repository.ScheduleRepository;
 
 @Service
 @RequiredArgsConstructor
 public class ScheduleServiceImpl implements ScheduleService{
 
+    private final ScheduleRepository scheduleRepository;
+
+    @Override
+    public ScheduleResponseDto createSchedule(String username, String title, String content) {
+        Schedule schedule = new Schedule(username, title, content);
+        Schedule savedSchedule = scheduleRepository.save(schedule);
+        return new ScheduleResponseDto(savedSchedule.getUsername(), savedSchedule.getTitle(), savedSchedule.getContent());
+    }
 }
