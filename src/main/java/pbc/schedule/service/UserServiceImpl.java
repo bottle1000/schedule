940324285e6ba0376033fd6b610cf4c5047a42b3
박병천 <genius178896@gmail.com>
2024-12-13 13:25:46 +0900

    fix : 암호화된 비밀번호 유저 객체에 넣어주는 코드 수정

--- src/main/java/pbc/schedule/service/UserServiceImpl.java
+++ src/main/java/pbc/schedule/service/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package pbc.schedule.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+import pbc.schedule.config.PasswordEncoder;
 import pbc.schedule.dto.response.LoginResponseDto;
 import pbc.schedule.dto.response.UserResponseDto;
 import pbc.schedule.entity.User;
@@ -12,11 +14,13 @@ import pbc.schedule.repository.UserRepository;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     /**
      * 유저 생성 기능(회원가입)
@@ -27,7 +31,13 @@ public class UserServiceImpl implements UserService{
      */
     @Override
     public UserResponseDto createUser(String username, String email, String password) {
-        User user = new User(username, email, password);
+
+        // 평문 비밀번호 암호화
+        String encodePassword = passwordEncoder.encode(password);
+        // 암호화된 비밀번호 확인
+        log.info("암호화 된 비밀번호 = {}", encodePassword);
+
+        User user = new User(username, email, encodePassword);
         User savedUser = userRepository.save(user);
 
         return new UserResponseDto(savedUser.getUsername(), savedUser.getEmail());
@@ -43,6 +53,7 @@ public class UserServiceImpl implements UserService{
     public LoginResponseDto login(String email, String password) {
         /**
          * 유저Repository 에서 email을 찾고 해당 유저가 있다면 그 유저를 반환 받음
+         * orElseThrow를 사용하여 Optional 에서 바로 객체를 받음
          */
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new NoSuchElementException("없는 이메일 입니다."));
@@ -50,7 +61,7 @@ public class UserServiceImpl implements UserService{
         /**
          * 반환 받은 유저 객체의 password 와 요청 받은 password 를 비교함
          */
-        if (!user.getPassword().equals(password)) {
+        if (!passwordEncoder.matches(password, user.getPassword())) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,"비밀빈호가 일치하지 않습니다.");
         }
 
