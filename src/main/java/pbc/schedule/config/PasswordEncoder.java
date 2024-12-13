51affe16ed71c23ac0153bc40d45798b18380650
박병천 <genius178896@gmail.com>
2024-12-13 09:47:39 +0900

    chore : 코드 주석 추가

--- src/main/java/pbc/schedule/config/PasswordEncoder.java
+++ src/main/java/pbc/schedule/config/PasswordEncoder.java
@@ -6,10 +6,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class PasswordEncoder {
 
+    /**
+     * 입력된 평문 비밀번호를 암호화 하는 메서드
+     * @param rawPassword : 유저로 부터 입력 받은 평문 비밀번호
+     * @return : 해쉬된 비밀번호를 반환
+     */
     public String encode(String rawPassword) {
         return BCrypt.withDefaults().hashToString(BCrypt.MIN_COST, rawPassword.toCharArray());
     }
 
+    /**
+     * 평문 비밀번호와 저장된 암호화된 비밀번호를 비교
+     * @param rawPassword
+     * @param encodedPassword
+     * @return
+     */
     public boolean matches(String rawPassword, String encodedPassword) {
         BCrypt.Result result = BCrypt.verifyer().verify(rawPassword.toCharArray(), encodedPassword);
         return result.verified;
