c32ba3cd12015ca19fdfd50208d66e2ec94a3753
박병천 <genius178896@gmail.com>
2024-12-14 21:53:07 +0900

    Chore : Comment Entity 초기 커밋

---
+++ src/main/java/pbc/schedule/entity/Comment.java
@@ -0,0 +1,42 @@
+package pbc.schedule.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "comment")
+public class Comment extends CommentBaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String commentContent;
+
+    /**
+     * 한 명의 유저는 여러 개의 댓글을 달 수 있다. 1 : N
+     */
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    /**
+     * 하나의 일정에는 여러 개의 댓글을 달 수 있다. 1 : N
+     */
+    @ManyToOne
+    @JoinColumn(name = "schedule_id")
+    private Schedule schedule;
+
+    public Comment() {
+    }
+
+    public void setSchedule(Schedule schedule) {
+        this.schedule = schedule;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
