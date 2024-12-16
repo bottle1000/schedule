7dc681dcb34eba05abbd2f9ba65985ddaba9d76c
박병천 <genius178896@gmail.com>
2024-12-16 19:22:26 +0900

    Add : 댓글 전체 조회 DTO 추가

---
+++ src/main/java/pbc/schedule/dto/response/FindAllCommentResponseDto.java
@@ -0,0 +1,15 @@
+package pbc.schedule.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import pbc.schedule.entity.Comment;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class FindAllCommentResponseDto {
+
+    private List<Comment> commentList;
+
+}
