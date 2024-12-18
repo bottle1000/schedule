594baad38b1a5ac124030447631e52e3091d4daa
박병천 <genius178896@gmail.com>
2024-12-18 17:50:28 +0900

    Add : 일정 전체 조회 (페이징 기능)

---
+++ src/main/java/pbc/schedule/dto/response/AllScheduleDto.java
@@ -0,0 +1,18 @@
+package pbc.schedule.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+public class AllScheduleDto {
+
+    private String title;
+    private String content;
+    private int commentCount;
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+    private String username;
+}
