7786540dd93e23c336f335f32dd6360935d6d822
박병천 <genius178896@gmail.com>
2024-12-31 10:41:56 +0900

    Feat : JSON 형태에 맞춰 에러 응답 및 로그 추가

--- src/main/java/pbc/schedule/exception/GlobalExceptionHandler.java
+++ src/main/java/pbc/schedule/exception/GlobalExceptionHandler.java
@@ -1,45 +1,61 @@
 package pbc.schedule.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(InvalidPasswordException.class)
-    public ResponseEntity<String> handlerInvalidPasswordException(InvalidPasswordException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+    public ResponseEntity<ErrorResponse> handlerInvalidPasswordException(InvalidPasswordException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), HttpStatus.UNAUTHORIZED.value());
+        log.error(e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(NotFoundAuthorException.class)
-    public ResponseEntity<String> handlerNotFoundAuthorException(NotFoundAuthorException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponse> handlerNotFoundAuthorException(NotFoundAuthorException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), HttpStatus.NOT_FOUND.value());
+        log.error(e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(NotFoundCalenderException.class)
-    public ResponseEntity<String> handlerNotFoundCalenderException(NotFoundCalenderException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponse> handlerNotFoundCalenderException(NotFoundCalenderException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), HttpStatus.NOT_FOUND.value());
+        log.error(e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(PageOutOfRangeException.class)
-    public ResponseEntity<String> handlerPageOutOfRangeException(PageOutOfRangeException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponse> handlerPageOutOfRangeException(PageOutOfRangeException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), HttpStatus.BAD_REQUEST.value());
+        log.error(e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(NotFoundEmailException.class)
-    public ResponseEntity<String> handlerNotFoundEmailException(NotFoundCalenderException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponse> handlerNotFoundEmailException(NotFoundEmailException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), HttpStatus.NOT_FOUND.value());
+        log.error(e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(NotFoundUserException.class)
-    public ResponseEntity<String> handlerNotFoundUserException(NotFoundUserException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponse> handlerNotFoundUserException(NotFoundUserException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), HttpStatus.NOT_FOUND.value());
+        log.error(e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(NotFoundScheduleIdException.class)
-    public ResponseEntity<String> NotFoundScheduleIdException(NotFoundScheduleIdException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponse> NotFoundScheduleIdException(NotFoundScheduleIdException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), HttpStatus.NOT_FOUND.value());
+        log.error(e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 }
